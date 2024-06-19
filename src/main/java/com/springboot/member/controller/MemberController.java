45fd520b2676ecb9de198a03b6daca6fdfbf9807
Tizesin <tlsalswns0o@gmail.com>
2024-06-19 18:12:36 +0900

    homework

--- src/main/java/com/springboot/member/controller/MemberController.java
+++ src/main/java/com/springboot/member/controller/MemberController.java
@@ -1,5 +1,6 @@
 package com.springboot.member.controller;
 
+import com.springboot.member.dto.MemberPageResponseDto;
 import com.springboot.member.dto.MemberPatchDto;
 import com.springboot.member.dto.MemberPostDto;
 import com.springboot.member.dto.MemberResponseDto;
@@ -8,6 +9,7 @@ import com.springboot.member.mapper.MemberMapper;
 import com.springboot.member.service.MemberService;
 import com.springboot.utils.UriCreator;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -71,10 +73,14 @@ public class MemberController {
     }
 
     @GetMapping
-    public ResponseEntity getMembers() {
+    public ResponseEntity getMembers(@Positive @RequestParam int page,
+                                     @Positive @RequestParam int size) {
         // TODO 페이지네이션을 적용하세요!
-        List<Member> members = memberService.findMembers();
-        List<MemberResponseDto> response = mapper.membersToMemberResponseDtos(members);
+        Page<Member> pageMembers = memberService.findMembers(page,size);
+        List<Member> members = pageMembers.getContent();
+        List<MemberResponseDto> responseDto = mapper.membersToMemberResponseDtos(members);
+
+        MemberPageResponseDto response = new MemberPageResponseDto(responseDto,pageMembers);
 
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
--- src/main/java/com/springboot/order/repository/OrderRepository.java
+++ src/main/java/com/springboot/order/repository/OrderRepository.java
@@ -1,6 +1,7 @@
 package com.springboot.order.repository;
 
 import com.springboot.order.entity.Order;
+import org.springframework.data.domain.Page;
 import org.springframework.data.repository.CrudRepository;
 
 public interface OrderRepository extends CrudRepository<Order, Long> {
