59070e909285bc7c97906a0da77e5b4c52845751
Rhino <qkdrb1234@naver.com>
2024-06-20 09:01:13 +0900

    feature controller

--- src/main/java/com/springboot/member/controller/MemberController.java
+++ src/main/java/com/springboot/member/controller/MemberController.java
@@ -1,13 +1,16 @@
 package com.springboot.member.controller;
 
+import com.springboot.response.MultiResponseDto;
 import com.springboot.member.dto.MemberPatchDto;
 import com.springboot.member.dto.MemberPostDto;
 import com.springboot.member.dto.MemberResponseDto;
 import com.springboot.member.entity.Member;
 import com.springboot.member.mapper.MemberMapper;
 import com.springboot.member.service.MemberService;
+import com.springboot.response.SingleResponseDto;
 import com.springboot.utils.UriCreator;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -66,15 +69,22 @@ public class MemberController {
     public ResponseEntity getMember(
             @PathVariable("member-id") @Positive long memberId) {
         Member response = memberService.findMember(memberId);
-        return new ResponseEntity<>(mapper.memberToMemberResponseDto(response)
+        return new ResponseEntity<>(new SingleResponseDto<>
+                (mapper.memberToMemberResponseDto(response))
                 , HttpStatus.OK);
     }
 
     @GetMapping
-    public ResponseEntity getMembers() {
+    public ResponseEntity getMembers(@Positive(message = "page는 0보다 큰 숫자만 가능합니다.")
+                                     @RequestParam(value = "page", defaultValue = "1") int page,
+                                     @Positive(message = "size는 0보다 큰 숫자만 가능합니다.")
+                                     @RequestParam(value = "size", defaultValue = "1") int size) {
         // TODO 페이지네이션을 적용하세요!
-        List<Member> members = memberService.findMembers();
-        List<MemberResponseDto> response = mapper.membersToMemberResponseDtos(members);
+        Page<Member> pageMembers = memberService.findMembers(page - 1, size);
+        List<Member> members = pageMembers.getContent();
+        List<MemberResponseDto> responseDto = mapper.membersToMemberResponseDtos(members);
+
+        MultiResponseDto response = new MultiResponseDto<>(responseDto, pageMembers);
 
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
